1d76fb64663c8bc63ec47cc860520d86671a3b09
suresh <bist.suresh@ymailcom>
2023-08-15 17:22:31 +0530

    Feat/movie-app: Added pg search to search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
   def index
+    @params = params
     @movies = Movie.all
-    @movies = @movies.where("? = ANY(actors)", params[:actors]) if params[:actorss]
+    @movies = @movies.search(params[:actor]) if params[:actor] # added after 20 minutes
     @movies = @movies.order(average_rating: :desc)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,13 @@
 class Movie < ApplicationRecord
+  include PgSearch::Model
   has_many :reviews
   validates :title, :description, :year, :director, :actors, :filming_location, :country, presence: true
+
+  pg_search_scope :search, against: {
+    actors: 'A',
+  }, using: {
+    tsearch: { prefix: true }
+  }
 
   def update_average_rating
     average_rating = reviews.average(:stars) || 0
