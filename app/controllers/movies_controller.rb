783db3080866a29efb2e79bcb0bf026b47d5c7f2
suresh <bist.suresh@ymailcom>
2023-08-15 16:00:49 +0530

    Feat/movie-app: Updated the views of movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.left_joins(:reviews).group(:id).order('AVG(reviews.stars) DESC NULLS LAST')
   end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,6 @@
 module MoviesHelper
+  def calculate_average_rating(movie)
+    average_rating = movie.reviews.average(:stars)
+    average_rating ? '%.2f' % average_rating : 'N/A'
+  end
 end
